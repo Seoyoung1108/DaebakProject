72dfc499e7a9b573c1a6bebf8535cbf8d136e5b0
Seoyoung Cho <83755941+Seoyoung1108@users.noreply.github.com>
2022-12-01 02:26:45 +0900

    Add files via upload

---
+++ static/js/deliverytime.js
@@ -0,0 +1,14 @@
+function deliverytime(clicked_name) {
+  var choose = document.getElementsByName(clicked_name)[0];
+  var dtime =choose.value;
+
+  for(var i=1; i<=12; i++){
+    var str = 'time' + i;
+    var target = document.getElementsByName(str)[0];
+    choose.style.backgroundColor='#585858';
+  } 
+  choose.style.backgroundColor='BE81F7';
+  
+
+  $('input[name=dtime]').attr('value',dtime);          
+}    
